a906727113050255df3ffccf7910f6ef340395cc
Swetneel <2101031000225@silveroakuni.ac.in>
2024-02-19 17:47:21 +0530

    save files

---
+++ main.py
@@ -0,0 +1,18 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/neurolab08")
+
+database = client["neurolab08"]
+
+collection = dataBase['Products']
+
+d = {'companyName':'ineuron',
+     'product': 'Affordable AI',
+     'courseOffered':'Machine Learning with Deployment'}
+
+rec = collection.insert_one(d)
+
+all_record = collection.find()
+
+for idx, record in enumurate(all_record):
+    print(f"{idx}: {record}")
